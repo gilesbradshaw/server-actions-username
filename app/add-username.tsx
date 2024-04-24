9d02eed78d937b154b0923cbd035da7c122deb02
giles <giles.bradshaw@sigyl.com>
2024-04-24 10:50:18 +0100

    redirect

--- app/add-username.tsx
+++ app/add-username.tsx
@@ -1,4 +1,7 @@
 "use server"
+
+import { redirect } from "next/navigation"
+
 type AddUsername = {
     success: false;
     error: string;
@@ -11,19 +14,5 @@ export async function addUsername(
     formData: FormData
 ): Promise<AddUsername> {
     const { username } = Object.fromEntries(formData);
-
-    if (!username || typeof username !== 'string' || username.length < 2) {
-        return {
-            success: false,
-            error: 'Username must be at a valid string of at least 2 characters'
-        };
-    }
-
-    // simulate adding to database
-    await new Promise((res) => setTimeout(res, 1000));
-
-    return {
-        success: true,
-        username: 'server-' + username
-    };
+    redirect(username.toString())
 }
